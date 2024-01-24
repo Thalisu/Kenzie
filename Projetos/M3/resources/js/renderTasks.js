5f08bf102f84452625fe71626e4e37a816749d4e
thalisu <thalisondmcc@gmail.com>
2024-01-24 13:41:27 -0300

    🧩 Tarefa 2 ✅

---
+++ Projetos/M3/resources/js/renderTasks.js
@@ -0,0 +1,38 @@
+const taskList = document.querySelector(".tasks__list");
+
+const createTaskContainer = (task) => {
+  const TaskContainer = document.createElement("div");
+  TaskContainer.className = "task-info__container";
+
+  const taskType = document.createElement("span");
+  taskType.className = `task-type span-${task.type}`;
+
+  const todoTask = document.createElement("p");
+  todoTask.textContent = task.content;
+
+  TaskContainer.appendChild(taskType);
+  TaskContainer.appendChild(todoTask);
+  return TaskContainer;
+};
+
+const deleteTask = () => {
+  const deleteTodoTask = document.createElement("button");
+  deleteTodoTask.className = "task__button--remove-task";
+
+  const trashIcon = document.createElement("i");
+  trashIcon.className = "fa-solid fa-trash";
+
+  deleteTodoTask.appendChild(trashIcon);
+  return deleteTodoTask;
+};
+
+for (let i = 0; i < tasks.length; i++) {
+  const task = tasks[i];
+  const taskItem = document.createElement("li");
+  taskItem.className = "task__item";
+
+  taskItem.appendChild(createTaskContainer(task));
+  taskItem.appendChild(deleteTask());
+
+  taskList.appendChild(taskItem);
+}
---
+++ Projetos/M3/resources/js/tasks.js
@@ -0,0 +1,12 @@
+const tasks = [
+  { content: "Comprar comida para o gato", type: "urgent" },
+  { content: "Consertar Computador", type: "important" },
+  { content: "Beber água", type: "normal" },
+  { content: "Enviar relatório trimestral", type: "important" },
+  { content: "Fazer exercícios físicos", type: "normal" },
+  { content: "Agendar consulta médica", type: "urgent" },
+  { content: "Ler pelo menos um capítulo de um livro", type: "normal" },
+  { content: "Limpar a despensa", type: "important" },
+  { content: "Pagar a conta de energia", type: "urgent" },
+  { content: "Assistir a um documentário interessante", type: "normal" },
+];
