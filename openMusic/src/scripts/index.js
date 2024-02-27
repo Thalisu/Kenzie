79d97c7b938bcf281523043612b172b37a32bfc4
thalisu <thalisondmcc@gmail.com>
2024-02-27 12:15:26 -0300

    projeto m4 finalizado

--- openMusic/src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import theme from "./theme.js";
-import handleButtons from "./genres/index.js";
-import handlePriceSlider from "./priceSlider/index.js";
-import createAlbums from "./albums/index.js";
-
-theme();
-handlePriceSlider();
-handleButtons();
-createAlbums();
